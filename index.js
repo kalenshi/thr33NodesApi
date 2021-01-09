4facaee2697d0e259b7d64a28bae6c354c937823
Kalenshi Katebe <kalenshi@gmail.com>
2021-01-08 23:48:43 -0400

    Started working od the Data access Object to connect to the mysql database. created the book endpoints for ading, listing and updating the books in the library added empty js and css files.  Changes to be committed: 	modified:   index.js 	new file:   public/css/style.css 	new file:   public/js/main.js 	new file:   public/js/register_worker.js 	new file:   routes/api/books/books.js 	new file:   routes/dao/dao.js 	modified:   views/home.ejs 	new file:   views/layout/footer.html 	new file:   views/layout/header.html

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const api = require("./routes/api/api.js");
 const homeHtml = require("./routes/html/home");
+const books = require("./routes/api/books/books");
+const {getConnection} = require("./routes/dao/dao");
 
 
 const app = express();
@@ -11,10 +13,14 @@ const PORT = process.env.PORt || 3000;
 //middleware
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
+app.use("/assets", express.static(`${__dirname}/public`));
 
 app.set('view engine', 'ejs');
 
+//database connections
+getConnection();
 app.use("/api", api);
+app.use("/api/books/", books);
 app.use("/", homeHtml);
 
 app.listen(PORT, ()=>{
